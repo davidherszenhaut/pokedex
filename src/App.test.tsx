a353b7ed1d104703dca0b5a52e5f57a4b72aa7c2
David Herszenhaut <david.herszenhaut@gmail.com>
2021-09-21 21:40:25 -0400

    test(App): reimplement App render test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,8 @@
-// import React from "react";
-// import { render, waitFor } from "@testing-library/react";
-// import App from "./App";
+import React from "react";
+import { render, waitFor } from "@testing-library/react";
+import App from "./App";
 
-// test("renders Pokémon list", async () => {
-//   const screen = render(<App />);
-//   await waitFor(() => screen.getByText("bulbasaur"));
-// });
-
-export {};
+test("renders Pokémon list", async () => {
+  const screen = render(<App />);
+  await waitFor(() => screen.getByText("Pokédex"));
+});
