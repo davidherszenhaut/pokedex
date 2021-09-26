83c5099221d702411d046a5f2a0182e60d78b1fb
David Herszenhaut <david.herszenhaut@gmail.com>
2021-09-25 21:59:43 -0400

    feat: add loading icon
    Add a loading icon to PokeList and PokeList item.
    
    Closes #32

--- src/PokeSearch/PokeSearch.tsx
+++ src/PokeSearch/PokeSearch.tsx
@@ -12,8 +12,10 @@ const PokeSearch = ({ setSearchText }: Props): ReactElement => {
   };
 
   return (
-    <form className="max-w-lg flex flex-row justify-around my-0 mx-auto items-center ">
-      <label className="text-gray-500 font-bold">Search for a pokémon!</label>
+    <form className="my-0 mx-auto flex justify-center items-center">
+      <label className="text-gray-500 font-bold pr-3">
+        Search for a pokémon!
+      </label>
       <input
         className="bg-gray-200 border-2 border-gray-200 rounded-md text-gray-700 px-2 focus:outline-none focus:border-purple-500 leading-tight py-2"
         type="text"
