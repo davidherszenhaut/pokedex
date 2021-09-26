d45a19c8246d8b3597e423e4946ff1f6f637eaf2
David Herszenhaut <david.herszenhaut@gmail.com>
2021-09-26 18:50:36 -0400

    feat: toggle silhouette styling

--- src/PokeDetail/PokeDetail.tsx
+++ src/PokeDetail/PokeDetail.tsx
@@ -102,7 +102,7 @@ const PokeDetail = ({
             leaveFrom="opacity-100 scale-100"
             leaveTo="opacity-0 scale-95"
           >
-            <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
+            <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white dark:bg-gray-800 dark:text-white shadow-xl rounded-2xl">
               {isLoading ? (
                 <div>
                   <img
@@ -122,7 +122,7 @@ const PokeDetail = ({
                 <div>
                   <Dialog.Title
                     as="h3"
-                    className="text-lg font-medium leading-6 text-gray-900"
+                    className="text-lg font-medium leading-6 text-gray-900 dark:text-white"
                   >
                     {capitalize(pokemonName)}, the {pokemonGenus}
                   </Dialog.Title>
--- src/PokeHeader/PokeHeader.tsx
+++ src/PokeHeader/PokeHeader.tsx
@@ -1,9 +1,51 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useState } from "react";
+import solrock from "../images/solrock.png";
+import solrockSilhouette from "../images/solrock-silhouette.png";
+import lunatone from "../images/lunatone.png";
+import lunatoneSilhouette from "../images/lunatone-silhouette.png";
 
 const PokeHeader = (): ReactElement => {
+  const [isLightMode, setIsLightMode] = useState<boolean>(true);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.target.checked ? setIsLightMode(false) : setIsLightMode(true);
+  };
+
   return (
-    <header className="text-4xl text-center py-6">
-      <h1>Pokédex</h1>
+    <header className="text-4xl text-center py-6 grid grid-cols-3">
+      <div></div>
+      <h1 className="self-center">Pokédex</h1>
+      <div className="flex justify-end items-center space-x-2">
+        <span className="text-sm text-gray-800 dark:text-gray-500">
+          <img
+            src={isLightMode ? solrock : solrockSilhouette}
+            alt="Light"
+            height={64}
+            width={64}
+          ></img>
+        </span>
+        <div>
+          <input
+            type="checkbox"
+            onChange={(e) => handleChange(e)}
+            className="hidden"
+            id="theme-toggle"
+          ></input>
+          <label htmlFor="theme-toggle">
+            <div className="w-9 h-5 flex items-center bg-gray-300 dark:bg-gray-600 rounded-full p-1">
+              <div className="toggle-dot w-4 h-4 bg-white rounded-full shadow-md transform duration-300 ease-in-out"></div>
+            </div>
+          </label>
+        </div>
+        <span className="text-sm text-gray-400 dark:text-gray-100">
+          <img
+            src={isLightMode ? lunatoneSilhouette : lunatone}
+            alt="Dark"
+            height={64}
+            width={64}
+          ></img>
+        </span>
+      </div>
     </header>
   );
 };
