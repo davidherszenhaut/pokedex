aeb1969706fffb45dcf57dba584dadc50c2e5da0
David Herszenhaut <david.herszenhaut@gmail.com>
2021-09-26 11:53:10 -0400

    feat(PokeDetail): retrieve pokemon species data

--- src/PokeDetail/PokeDetail.tsx
+++ src/PokeDetail/PokeDetail.tsx
@@ -1,6 +1,6 @@
 import React, { ReactElement, useEffect, useState } from "react";
 import { Dialog } from "@headlessui/react";
-import { BASE_URL } from "../utils";
+import { BASE_URL, capitalize } from "../utils";
 
 type SetIsOpen = (isOpen: boolean) => void;
 
@@ -10,32 +10,76 @@ interface Props {
   setIsOpen: SetIsOpen;
 }
 
+interface FlavorTextEntry {
+  flavor_text: string;
+  language: Record<string, string>;
+  version: Record<string, string>;
+}
+
+interface FlavorText {
+  version: string;
+  text: string;
+}
+
 const PokeDetail = ({
   pokemonNumber,
   isOpen,
   setIsOpen,
 }: Props): ReactElement => {
   const [pokemonName, setPokemonName] = useState<string>("");
-  const [pokemonSpriteUrl, setPokeSpriteUrl] = useState<string>("");
+  // const [pokemonSpriteUrl, setPokemonSpriteUrl] = useState<string>("");
+  const [pokemonFlavorTexts, setPokemonFlavorTexts] = useState<FlavorText[]>(
+    []
+  );
+  const [pokemonGenus, setPokemonGenus] = useState<string>("");
+  const [pokemonGeneration, setPokemonGeneration] = useState<string>("");
+  const [pokemonHabitat, setPokemonHabitat] = useState<string>("");
 
   useEffect(() => {
     fetch(`${BASE_URL}pokemon/${pokemonNumber}`)
       .then((response) => response.json())
       .then((data) => {
-        setPokeSpriteUrl(data.sprites.front_default);
+        // setPokemonSpriteUrl(data.sprites.front_default);
         setPokemonName(data.name);
+      });
+    fetch(`${BASE_URL}pokemon-species/${pokemonNumber}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setPokemonFlavorTexts(
+          data.flavor_text_entries
+            .filter((entry: FlavorTextEntry) => entry.language.name === "en")
+            .map((entry: FlavorTextEntry) => ({
+              version: entry.version.name,
+              text: entry.flavor_text,
+            }))
+        );
+        setPokemonGenus(
+          data.genera.filter(
+            (entry: FlavorTextEntry) => entry.language.name === "en"
+          )[0].genus
+        );
+        setPokemonGeneration(data.generation.name.split("-")[1].toUpperCase());
+        data.habitat && data.habitat.name
+          ? setPokemonHabitat(data.habitat.name)
+          : setPokemonHabitat("N/A");
       });
   }, [pokemonNumber]);
 
   return (
     <Dialog
+      as="div"
       open={isOpen}
       onClose={() => setIsOpen(false)}
-      className="border-2 border-red-800 fixed inset-0 bg-black opacity-80 flex justify-center items-center"
+      className="fixed inset-0 z-10 overflow-y-auto"
     >
-      <Dialog.Overlay />
-      <Dialog.Title className="text-red-50">{pokemonName}</Dialog.Title>
-      <Dialog.Description>{pokemonSpriteUrl}</Dialog.Description>
+      <Dialog.Overlay className="fixed inset-0" />
+      <Dialog.Title>{capitalize(pokemonName)}</Dialog.Title>
+      {pokemonFlavorTexts.length > 0 ? (
+        <p>{pokemonFlavorTexts[0].text}</p>
+      ) : null}
+      <p>{pokemonGenus}</p>
+      <p>{pokemonGeneration}</p>
+      <p>{pokemonHabitat}</p>
       <button onClick={() => setIsOpen(false)}>Close</button>
     </Dialog>
   );
